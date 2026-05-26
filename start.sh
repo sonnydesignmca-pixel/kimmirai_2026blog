55bfb6743a7345172174a053327d91d04c7b28f3
kimmirai <sonny.design.mca@gmail.com>
2026-05-26 13:52:04 +0900

    storageをlinkさせた

--- start.sh
+++ start.sh
@@ -15,4 +15,7 @@ php artisan vendor:publish --provider="CloudinaryLabs\CloudinaryLaravel\Cloudina
 echo "Running migrations..."
 php artisan migrate --force
 
+echo "Publishing storage..."
+php artisan storage:link
+
 php artisan serve --host=0.0.0.0 --port=$PORT
