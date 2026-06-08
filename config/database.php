78b714b531eeecc5c3927a254ad3fa022f4a1405
kimmirai <sonny.design.mca@gmail.com>
2026-06-08 14:45:28 +0900

    データベース接続を継続的にした

--- config/database.php
+++ config/database.php
@@ -97,6 +97,9 @@ return [
             'prefix_indexes' => true,
             'search_path' => 'public',
             'sslmode' => env('DB_SSLMODE', 'prefer'),
+            'options' => [
+                PDO::ATTR_PERSISTENT => true, // 👈 これを追加
+            ],
         ],
 
         'sqlsrv' => [
